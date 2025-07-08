6a39b311696dd84577b43b70581055786032b62f
Dewa Jayon <dewajayon3@gmail.com>
2025-07-08 14:20:46 +0800

    fix (middleware): fix middleware in controller Category and Supplier

---
+++ app/Http/Controllers/Dashboard/ProductController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use Illuminate\Support\Facades\Auth;
+
+class ProductController extends Controller
+{
+    /**
+     * Constructor for CategoryController.
+     *
+     * Checks if the user is logged in and has an admin role.
+     * If not, aborts with a 403 status code.
+     */
+    public function __construct()
+    {
+        if (Auth::check() && Auth::user()->role !== 'admin' && Auth::user()->role !== 'warehouse') {
+            abort(403);
+        }
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductRequest $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,24 @@
 
 use App\Http\Controllers\Dashboard\CategoryController;
 use App\Http\Controllers\Dashboard\DashboardController;
+use App\Http\Controllers\Dashboard\ProductController;
 use App\Http\Controllers\Dashboard\SupplierController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\IsAdmin;
+use App\Http\Middleware\IsWarehouse;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
+    // dashboard & general route
     Route::get('/', [DashboardController::class, 'index'])->name('home');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    // master data route
     Route::resource('/category', CategoryController::class);
     Route::resource('/supplier', SupplierController::class);
+    Route::resource('/product', ProductController::class);
 });
 
 
