0fa7e029262b4a70b9dda9a4061fb1ef1f39391f
Dewa Jayon <dewajayon3@gmail.com>
2025-08-29 23:56:14 +0800

    feat (stock-report): filter stock report

---
+++ app/Http/Controllers/Dashboard/StockReportController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Supplier;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class StockReportController extends Controller
+{
+
+    /**
+     * Handle stock report filtering and rendering.
+     * 
+     * The function expects category, supplier, start_date, and end_date parameters
+     * to be passed in the request. It will filter products based on the given
+     * parameters and return an Inertia response with the filtered products data.
+     * 
+     * @param \Illuminate\Http\Request $request
+     * 
+     * @return \Inertia\Response
+     */
+    public function index(Request $request)
+    {
+        $filters = [
+            'category'      => $request->category,
+            'supplier'      => $request->supplier,
+            'start_date'    => $request->start_date,
+            'end_date'      => $request->end_date
+        ];
+
+        $filteredStocks = [];
+
+        $query = collect();
+
+        if ($filters['category'] || $filters['supplier'] || $filters['start_date'] || $filters['end_date']) {
+
+            $query = Product::with(['category', 'purchaseItems.purcase.supplier'])
+                ->whereHas('purchaseItems')
+                ->when($filters['category'] && $filters['category'] !== 'semua-kategori', function ($query) use ($filters) {
+                    return $query->where('category_id', $filters['category']);
+                })
+                ->when($filters['supplier'] && $filters['supplier'] !== 'semua-supplier', function ($query) use ($filters) {
+                    return $query->whereHas('purchaseItems.purcase.supplier', function ($q) use ($filters) {
+                        $q->where('id', $filters['supplier']);
+                    });
+                })
+                ->when($filters['start_date'], function ($query) use ($filters) {
+                    return $query->whereHas('purchaseItems.purcase', function ($q) use ($filters) {
+                        $q->whereDate('date', '>=', $filters['start_date']);
+                    });
+                })
+                ->when($filters['end_date'], function ($query) use ($filters) {
+                    return $query->whereHas('purchaseItems.purcase', function ($q) use ($filters) {
+                        $q->whereDate('date', '<=', $filters['end_date']);
+                    });
+                })
+                ->paginate(10)
+                ->withQueryString();
+
+            $filteredStocks = $query;
+        }
+
+        return Inertia::render('Reports/Stock/Index', [
+            'filteredStocks'    => $filteredStocks
+        ]);
+    }
+
+
+    /**
+     * Search category by name
+     * 
+     * @param \Illuminate\Http\Request $request
+     * 
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function searchCategory(Request $request)
+    {
+        if ($request->expectsJson()) {
+
+            $query = Category::query()
+                ->where('name', 'like', '%' . $request->search . '%')
+                ->limit(5)
+                ->get();
+
+            if ($query->isEmpty()) {
+                return response()->json([
+                    "message"   => "category not found",
+                    "data"      => null
+                ], 404);
+            }
+
+            return response()->json([
+                "message"   => "success",
+                "data"      => $query
+            ], 200);
+        }
+    }
+
+
+    /**
+     * Search supplier by name.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function searchSupplier(Request $request)
+    {
+        if ($request->expectsJson()) {
+
+            $query = Supplier::query()
+                ->where('name', 'like', '%' . $request->search . '%')
+                ->limit(5)
+                ->get();
+
+            if ($query->isEmpty()) {
+                return response()->json([
+                    "message"   => "supplier not found",
+                    "data"      => null
+                ], 404);
+            }
+
+            return response()->json([
+                "message"   => "success",
+                "data"      => $query
+            ]);
+        }
+    }
+}
---
+++ resources/js/Pages/Reports/Stock/partials/Filter.vue
@@ -0,0 +1,327 @@
+<script setup>
+import DatePicker from "@/Components/DatePicker.vue";
+import { Button } from "@/Components/ui/button";
+import { Label } from "@/Components/ui/label";
+import { ref, watch } from "vue";
+import { router } from "@inertiajs/vue3";
+import { cn } from "@/lib/utils";
+import { watchDebounced } from "@vueuse/core";
+import axios, { all } from "axios";
+import { toast } from "vue-sonner";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/Components/ui/card";
+import { Check, FileSpreadsheet, Printer, Search } from "lucide-vue-next";
+
+import {
+    Combobox,
+    ComboboxAnchor,
+    ComboboxEmpty,
+    ComboboxGroup,
+    ComboboxInput,
+    ComboboxItem,
+    ComboboxItemIndicator,
+    ComboboxList,
+} from "@/Components/ui/combobox";
+import { Checkbox } from "@/Components/ui/checkbox";
+
+const formatDate = (date) => {
+    if (!date) return null;
+    const d = new Date(date);
+    const year = d.getFullYear();
+    const month = String(d.getMonth() + 1).padStart(2, "0");
+    const day = String(d.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+};
+
+const startDate = ref(null);
+const endDate = ref(null);
+const selectedCategory = ref(null);
+const selectedSupplier = ref(null);
+const allCategory = ref(false);
+const allSupplier = ref(false);
+
+watch(selectedCategory, (newVal) => {
+    if (newVal) {
+        allSupplier.value = false;
+    }
+});
+watch(allCategory, (newVal) => {
+    if (newVal) {
+        selectedCategory.value = null;
+    }
+});
+watch(selectedSupplier, (newVal) => {
+    if (newVal) {
+        allSupplier.value = false;
+    }
+});
+watch(allSupplier, (newVal) => {
+    if (newVal) {
+        selectedSupplier.value = null;
+    }
+});
+
+const categories = ref([]);
+const searchCategory = ref("");
+
+watchDebounced(
+    searchCategory,
+    (newSearchCategory) => {
+        axios
+            .get(
+                route("reports.stock.searchCategory", {
+                    search: newSearchCategory,
+                })
+            )
+            .then((response) => {
+                categories.value = response.data.data.map((item) => {
+                    return {
+                        label: item.name,
+                        value: item.slug,
+                    };
+                });
+            })
+            .catch((error) => {
+                categories.value = [];
+            });
+    },
+    { debounce: 500 }
+);
+
+const searchSupplier = ref("");
+const suppliers = ref([]);
+
+watchDebounced(
+    searchSupplier,
+    (newSearchSupplier) => {
+        axios
+            .get(
+                route("reports.stock.searchSupplier", {
+                    search: newSearchSupplier,
+                })
+            )
+            .then((response) => {
+                suppliers.value = response.data.data.map((item) => {
+                    return {
+                        label: item.name,
+                        value: item.slug,
+                    };
+                });
+            })
+            .catch((error) => {
+                suppliers.value = [];
+            });
+    },
+    { debounce: 500 }
+);
+
+const checkFilter = () => {
+    if (!startDate.value || !endDate.value) {
+        toast.error("Tanggal mulai dan tanggal selesai wajib diisi!");
+        return false;
+    }
+
+    if (!allSupplier.value && !selectedSupplier.value) {
+        toast.error("Silahkan pilih supplier atau centang Semua Supplier!");
+        return false;
+    }
+
+    if (!allCategory.value && !selectedCategory.value) {
+        toast.error("Silahkan pilih kategori atau centang Semua Kategori!");
+        return false;
+    }
+
+    return true;
+};
+const handleFilter = () => {
+    if (!checkFilter()) return;
+
+    let supplierParam = null;
+    let categoryParam = null;
+
+    allSupplier.value == true
+        ? (supplierParam = "semua-supplier")
+        : (supplierParam = selectedSupplier.value.value);
+    allCategory.value == true
+        ? (categoryParam = "semua-kategori")
+        : (categoryParam = selectedCategory.value.value);
+
+    router.get(route("reports.stock.index"), {
+        start_date: formatDate(startDate.value),
+        end_date: formatDate(endDate.value),
+        supplier: supplierParam,
+        category: categoryParam,
+    });
+};
+
+const handleExportPdf = () => {
+    alert("export pdf");
+};
+
+const handleExportExcel = () => {
+    alert("export excel");
+};
+</script>
+
+<template>
+    <Card>
+        <CardHeader>
+            <CardTitle>
+                <h1 class="text-2xl font-bold">Laporan Stock</h1>
+            </CardTitle>
+        </CardHeader>
+        <CardContent class="grid grid-cols-1 md:grid-cols-4 gap-4">
+            <div class="space-y-1">
+                <Label html-for="startDate">Tanggal Mulai</Label>
+                <DatePicker
+                    label="Tanggal"
+                    class="w-full"
+                    id="startDate"
+                    v-model="startDate"
+                />
+            </div>
+            <div class="space-y-1">
+                <Label html-for="endDate">Tanggal Selesai</Label>
+                <DatePicker
+                    label="Tanggal"
+                    class="w-full"
+                    id="endDate"
+                    v-model="endDate"
+                />
+            </div>
+            <div class="space-y-1">
+                <Label html-for="category">Kategori</Label>
+                <Combobox
+                    by="label"
+                    v-model="selectedCategory"
+                    html-id="category"
+                >
+                    <ComboboxAnchor class="w-full">
+                        <div class="relative w-full max-w-sm items-center">
+                            <ComboboxInput
+                                class="pl-9"
+                                :display-value="(val) => val?.label ?? ''"
+                                placeholder="Cari Kategori..."
+                                v-model="searchCategory"
+                            />
+                            <span
+                                class="absolute start-0 inset-y-0 flex items-center justify-center px-3"
+                            >
+                                <Search class="size-4 text-muted-foreground" />
+                            </span>
+                        </div>
+                    </ComboboxAnchor>
+
+                    <ComboboxList>
+                        <ComboboxEmpty>
+                            Tidak ada Kategori ditemukan.
+                        </ComboboxEmpty>
+
+                        <ComboboxGroup>
+                            <ComboboxItem
+                                v-for="category in categories"
+                                :key="category.value"
+                                :value="category"
+                                class="cursor-pointer"
+                            >
+                                {{ category.label }}
+
+                                <ComboboxItemIndicator>
+                                    <Check :class="cn('ml-auto h-4 w-4')" />
+                                </ComboboxItemIndicator>
+                            </ComboboxItem>
+                        </ComboboxGroup>
+                    </ComboboxList>
+                </Combobox>
+                <div class="items-top flex gap-x-2 pt-2">
+                    <Checkbox id="all-category" v-model="allCategory" />
+                    <div class="grid gap-1.5 leading-none">
+                        <label
+                            for="all-category"
+                            class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                        >
+                            Semua Kategori
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div class="space-y-1">
+                <Label html-for="supplier">Supplier</Label>
+                <Combobox
+                    by="label"
+                    v-model="selectedSupplier"
+                    html-id="supplier"
+                >
+                    <ComboboxAnchor class="w-full">
+                        <div class="relative w-full max-w-sm items-center">
+                            <ComboboxInput
+                                class="pl-9"
+                                :display-value="(val) => val?.label ?? ''"
+                                placeholder="Cari Supplier..."
+                                v-model="searchSupplier"
+                            />
+                            <span
+                                class="absolute start-0 inset-y-0 flex items-center justify-center px-3"
+                            >
+                                <Search class="size-4 text-muted-foreground" />
+                            </span>
+                        </div>
+                    </ComboboxAnchor>
+
+                    <ComboboxList>
+                        <ComboboxEmpty>
+                            Tidak ada Supplier ditemukan.
+                        </ComboboxEmpty>
+
+                        <ComboboxGroup>
+                            <ComboboxItem
+                                v-for="supplier in suppliers"
+                                :key="supplier.value"
+                                :value="supplier"
+                                class="cursor-pointer"
+                            >
+                                {{ supplier.label }}
+
+                                <ComboboxItemIndicator>
+                                    <Check :class="cn('ml-auto h-4 w-4')" />
+                                </ComboboxItemIndicator>
+                            </ComboboxItem>
+                        </ComboboxGroup>
+                    </ComboboxList>
+                </Combobox>
+                <div class="items-top flex gap-x-2 pt-2">
+                    <Checkbox id="all-supplier" v-model="allSupplier" />
+                    <div class="grid gap-1.5 leading-none">
+                        <label
+                            for="all-supplier"
+                            class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                        >
+                            Semua Supplier
+                        </label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex space-x-2">
+                <Button @click="handleFilter">
+                    <Search class="h-4 w-4" />
+                    <span>Lihat Laporan</span>
+                </Button>
+                <Button
+                    @click="handleExportPdf()"
+                    class="disabled:cursor-not-allowed disabled:opacity-50"
+                >
+                    <Printer class="h-4 w-4" />
+                    <span>Print PDF</span>
+                </Button>
+                <Button
+                    @click="handleExportExcel()"
+                    class="disabled:cursor-not-allowed disabled:opacity-50"
+                >
+                    <FileSpreadsheet class="h-4 w-4" />
+                    <span>Export Excel</span>
+                </Button>
+            </div>
+        </CardContent>
+    </Card>
+</template>
